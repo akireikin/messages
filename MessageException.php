48662966dfc901a50e9c328df68299184bf1c1a0
Aleksei Akireikin <opexus@gmail.com>
2015-08-14 19:23:50 +0300

    Create MessageException.php

---
+++ MessageException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MessageException.php
+ *
+ * @author Aleksei Akireikin <opexus@gmail.com>
+ */
+
+namespace app\core\messages;
+
+/**
+ * Class MessageException
+ *
+ * @package app\core\messages
+ */
+class MessageException extends \Exception {}
